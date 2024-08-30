f17bb7ba3c1fc4792c574e2b5ee0eb20d9292a83
Abdisalam Abdulahi <iidlex99@gmail.com>
2024-08-30 07:09:11 +0300

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes 'phpp' fulenames to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
